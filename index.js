c22fd8350629a44d411593f98c09377e2db3d080
adityasrivastava29 <adityasrivastava.niet@gmail.com>
2025-08-15 16:42:11 +0530

    added my frontend url for cors

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:5175",
+      "https://adityasri.in",
       "https://ai-persona-custom-chat-app-demo1-2cue6xzl9.vercel.app",
       /\.vercel\.app$/,
     ],
